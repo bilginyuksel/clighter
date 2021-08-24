b365348362c7f5b99b2c96d57d24bb5ee8244818
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-08-25 01:00:29 +0300

    feat! gitignore added and redundant function deleted

--- cli/engine.py
+++ cli/engine.py
@@ -7,8 +7,5 @@ class CLIEngine(Thread, Engine):
         Thread.__init__(self, group=None, target=self.start, name='cli_engine', args=self, kwargs=None, daemon=None)
         Engine.__init__(self, scene)
     
-    def start(self) -> None:
-        return super().start() 
-
     def run(self) -> None:
         self._start()
