9eeabcde92a48556a0f3446127fbf9bfdeb3c114
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-08-28 12:29:46 +0300

    refactor! cli code moved to main.py file

--- cli/game.py
+++ cli/game.py
@@ -1,5 +1,4 @@
-from core.factory import GameObjectFactory
-from core.scene import Dimension, GameObject, Position
+from core.scene import Dimension
 from core.game import Game
 
 from cli.scene import CLIScene
@@ -18,7 +17,6 @@ class CLIGame(Game):
     def start(self):
         self.engine.start()
         self.channel.start()
-        self._prepare()
 
     def pause(self):
         return super().pause()
@@ -32,12 +30,6 @@ class CLIGame(Game):
     def exit(self):
         self.engine.stop()
 
-    def _prepare(self):
-        # Create new character class
-        character = Character(Position(10, 10), Dimension(100, 200))
-        self.factory.extend_game_object(character, channel=True, scene=True)
-        character.factory = self.factory
-
     def _create_channel_callbacks(self):
         return {
             'q': self.exit,
@@ -45,37 +37,3 @@ class CLIGame(Game):
             'r': self.resume,
             'p': self.pause
         }
-
-
-class Character(GameObject):
-    def __init__(self, position: Position, dimension: Dimension) -> None:
-        super().__init__(position, dimension, filepath='assets/character.txt',
-                         obstacle=False, controllable=True)
-        self.factory: GameObjectFactory = None
-
-    def on_key_pressed(self, key: chr):
-        if key == 's':
-            self.position.x += 3
-        elif key == 'w':
-            self.position.x -= 3
-        elif key == 'd':
-            self.position.y += 3
-        elif key == 'a':
-            self.position.y -= 3
-        elif key == 'm':
-            bullet = Bullet(Position(self.position.x+2, self.position.y+5),
-                            Dimension(100, 200))
-            self.factory.extend_game_object(bullet, scene=True)
-
-
-class Bullet(GameObject):
-    def __init__(self, position: Position, dimension=None) -> None:
-        super().__init__(position, dimension=None, filepath='assets/bullet.txt',
-                         obstacle=False, controllable=False)
-        self.velocity = 0
-
-    def update(self):
-        self.velocity += 0.5
-        if self.velocity >= 1:
-            self.position.y += 1
-            self.velocity = 0
--- main.py
+++ main.py
@@ -1,4 +1,44 @@
+from core.scene import GameObject, Position, Dimension
+from core.factory import GameObjectFactory
 from cli.game import CLIGame
 
+
+class Character(GameObject):
+    def __init__(self, position: Position, dimension: Dimension) -> None:
+        super().__init__(position, dimension, filepath='assets/character.txt',
+                         obstacle=False, controllable=True)
+        self.factory: GameObjectFactory = None
+
+    def on_key_pressed(self, key: chr):
+        if key == 's':
+            self.position.x += 3
+        elif key == 'w':
+            self.position.x -= 3
+        elif key == 'd':
+            self.position.y += 3
+        elif key == 'a':
+            self.position.y -= 3
+        elif key == 'm':
+            bullet = Bullet(Position(self.position.x+2, self.position.y+5),
+                            Dimension(100, 200))
+            self.factory.extend_game_object(bullet, scene=True)
+
+
+class Bullet(GameObject):
+    def __init__(self, position: Position, dimension=None) -> None:
+        super().__init__(position, dimension=None, filepath='assets/bullet.txt',
+                         obstacle=False, controllable=False)
+        self.velocity = 0
+
+    def update(self):
+        self.velocity += 0.5
+        if self.velocity >= 1:
+            self.position.y += 1
+            self.velocity = 0
+
+
 g = CLIGame()
+character = Character(Position(10, 10), Dimension(100, 200))
+g.factory.extend_game_object(character, channel=True, scene=True)
+character.factory = g.factory
 g.start()
