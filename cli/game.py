9d2d4db626185abc3e9de9fb2d2dccf733b116b5
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-08-28 15:49:22 +0300

    feat(engine): cli engine removed and core engine used

--- cli/game.py
+++ cli/game.py
@@ -1,15 +1,15 @@
 from core.scene import Dimension
 from core.game import Game
+from core.engine import Engine
 
 from cli.scene import CLIScene
 from cli.input import CLIInputChannel
-from cli.engine import CLIEngine
 
 
 class CLIGame(Game):
     def __init__(self) -> None:
         self.scene = CLIScene(Dimension(70, 50))
-        self.engine = CLIEngine(self.scene, fps=60)
+        self.engine = Engine(self.scene, fps=60)
         self.channel = CLIInputChannel(self._create_channel_callbacks())
 
         super().__init__(self.scene, self.engine, self.channel)
--- core/engine.py
+++ core/engine.py
@@ -2,24 +2,33 @@ import time
 import logging
 
 from core.scene import GameObject, Scene
+from threading import Thread
 
 PAUSE = 'PAUSE'
 PLAY = 'PLAY'
 STOP = 'STOP'
 
-class Engine:
-    def __init__(self, scene: Scene, fps= 60) -> None:
-        self.logger = logging.Logger(self.__class__.__name__, level=logging.WARN)
+
+class Engine(Thread):
+    def __init__(self, scene: Scene, fps=60) -> None:
+        self.logger = logging.Logger(
+            self.__class__.__name__, level=logging.WARN)
         self.status = PAUSE
         self.scene = scene
         self.fps = fps
-    
+
+        Thread.__init__(self, group=None, target=self.start,
+                        name='cli_engine', args=self, kwargs=None, daemon=None)
+
+    def run(self) -> None:
+        return self._start()
+
     def _start(self):
         self.status = PLAY
         while self.status == PLAY:
             self.update()
             time.sleep(1/self.fps)
-    
+
     def stop(self):
         self.status = STOP
 
@@ -28,13 +37,13 @@ class Engine:
         self._update_per_frame()
         self._detect_collision()
         self._draw_frame()
-    
+
     def _update_per_frame(self):
         game_objects = self.scene.objects
         for game_object in game_objects.values():
             game_object.update()
         self.logger.info('frame updated.')
-    
+
     def _detect_collision(self):
         def is_intersected(o1: GameObject, o2: GameObject) -> bool:
             o1_left, o1_right = o1.position.x, o1.position.x + o1.dimension.width
@@ -46,14 +55,15 @@ class Engine:
             is_x_axis_intersected = o1_right <= o2_left or o2_right <= o1_left
             is_y_axis_intersected = o1_top <= o2_down or o2_top <= o1_down
             return not (is_x_axis_intersected or is_y_axis_intersected)
-    
+
         game_objects = list(self.scene.objects.values())
         for i in range(len(game_objects)):
             o1 = game_objects[i]
             for j in range(i+1, len(game_objects)):
                 o2 = game_objects[j]
                 if is_intersected(o1, o2):
-                    self.logger.info('collision detected between, o1= %s, o2= %s' % (o1.__name__, o2.__name__))
+                    self.logger.info('collision detected between, o1= %s, o2= %s' % (
+                        o1.__name__, o2.__name__))
                     o1.collide(o2)
                     o2.collide(o1)
 
