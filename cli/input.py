935fc51b5b58ed92aad442e18c829d9635fd3292
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-08-28 22:39:10 +0300

    refactor(input): cli input channel refactored

--- cli/input.py
+++ cli/input.py
@@ -4,21 +4,30 @@ import msvcrt
 
 
 class CLIInputChannel(Thread, InputChannel):
-    def __init__(self, special_input_callbacks) -> None:
+    def __init__(self, special_input_callbacks, game_over_input='q') -> None:
         Thread.__init__(self, group=None, target=self.start,
                         name='cli_input_channel', args=self, kwargs=None, daemon=None)
         InputChannel.__init__(self, special_input_callbacks)
+
+        self._game_over_input = game_over_input
 
     def run(self) -> None:
         self.open()
 
     def open(self):
-        while self._input != 'q':
-            self._input = msvcrt.getch().decode('ascii')
-            if self._input in self._callbacks:
-                self._callbacks[self._input]()
-            else:
-                self.notify_all(self._input)
+        current_input = None
+        while current_input != self._game_over_input:
+            try:
+                current_input = msvcrt.getch().decode('ascii')
+                if current_input in self._callbacks:
+                    self._callbacks[current_input]()
+                else:
+                    self.notify_all(current_input)
+            except:
+                # ascii decode error may happen
+                # no need to handle this case
+                # but the program should not crash.
+                pass
 
     def subscribe(self, game_object):
         self._subscribers[game_object.get_id()] = game_object
@@ -26,8 +35,3 @@ class CLIInputChannel(Thread, InputChannel):
     def notify_all(self, key: chr):
         for subscriber in self._subscribers.values():
             subscriber.on_key_pressed(key)
-
-    def get_input(self):
-        current_input = self._input
-        self._input = None
-        return current_input
