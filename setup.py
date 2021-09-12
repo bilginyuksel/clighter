a8502ddc59f3f868d42968a3c351b4b2f2300a28
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-09-12 23:09:01 +0300

    Package prepared for installation to package index

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clighter",
+    version="0.0.1",
+    author="bilginyuksel",
+    author_email="bilgin.yuksel96@gmail.com",
+    description="Game engine for CLI games.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bilginyuksel/clighter",
+    project_urls={
+        "Bug Tracker": "https://github.com/bilginyuksel/clighter/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "clighter"},
+    packages=setuptools.find_packages(where="clighter"),
+    python_requires=">=3.6",
+)
